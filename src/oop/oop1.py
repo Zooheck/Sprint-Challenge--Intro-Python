d42cb05a6e2f17b3e16c5fa759757751d8a4eaa2
Zooheck <davidscottflack@gmail.com>
2019-04-26 10:39:17 -0500

    Solve oop1

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,35 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+class Vehicle:
+    pass
+
+# GROUND VEHICLES
+
+
+class GroundVehicle(Vehicle):
+    pass
+
+
+class Car(GroundVehicle):
+    pass
+
+
+class Motorcycle(GroundVehicle):
+    pass
+
+    # FLIGHT VEHICLES
+
+
+class FlightVehicle(Vehicle):
+    pass
+
+
+class Starship(FlightVehicle):
+    pass
+
+
+class Airplane(FlightVehicle):
+    pass
